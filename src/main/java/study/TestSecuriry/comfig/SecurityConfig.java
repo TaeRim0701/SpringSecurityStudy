a211ab266231ee070e979c6915f5a967db6951c0
TaeRim0701 <xofla71@naver.com>
2025-07-08 17:51:50 +0900

    세션 설정

--- src/main/java/study/TestSecuriry/comfig/SecurityConfig.java
+++ src/main/java/study/TestSecuriry/comfig/SecurityConfig.java
@@ -33,8 +33,23 @@ public class SecurityConfig {
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
+
         http
                 .csrf((auth) -> auth.disable()); //개발 환경에서만 csrf 비홠성화
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1) // 하나의 아이디에 대한 다중 로그인 허용 개수
+                        .maxSessionsPreventsLogin(true));  // 다중 로그인 개수를 초과하였을 경우 처리 방법
+                                                           // true : 초과시 새로운 로그인 차단, false : 초과시 기존 세션 하나 삭제
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());
+        // 세션 고정 공격을 보호하기 위한 로그인 성공시 세션 설정 방법은 sessionManagement() 메소드의 sessionFixation() 메소드를 통해서 설정
+        //sessionManagement().sessionFixation().none() : 로그인 시 세션 정보 변경 안함
+        //sessionManagement().sessionFixation().newSession() : 로그인 시 세션 새로 생성
+        //sessionManagement().sessionFixation().changeSessionId() : 로그인 시 동일한 세션에 대한 id 변경
         
         return http.build();
     }
